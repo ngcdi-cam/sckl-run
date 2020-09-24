87e7a6a8bb19e161add84da29b6ad1f4c50ddcc3
Peter <mbyzhang@outlook.com>
2020-09-24 15:57:23 +0000

    full agent topologies

--- run_experiments.py
+++ run_experiments.py
@@ -17,13 +17,11 @@ NETWORK_CONFIG = 'configs/network.overlay.yaml'
 AGENTS_CONFIG = 'configs/agents.overlay.yaml'
 NO_SCKL_DATA_CONFIG = 'configs/no_sckl_data.overlay.yaml'
 NO_SCKL_SM_CONFIG = 'configs/agents_nosm.overlay.yaml'
-
-MININET_COMPLETE_BIPARTITE_TOPO = 'configs/mininet_topos/complete_bipartite.yaml'
-MININET_CUSTOM_TOPO = 'configs/mininet_topos/custom.yaml'
-MININET_MESH_TOPO = 'configs/mininet_topos/mesh.yaml'
-MININET_RING_TOPO = 'configs/mininet_topos/ring.yaml'
+NO_NETWORK_CONFIG = 'configs/no_network.overlay.yaml'
 
 MININET_TOPO_BASE_DIR = 'configs/mininet_topos'
+AGENT_TOPO_BASE_DIR = 'configs/agent_topos'
+
 
 def get_ip_address_of_container(name):
     return subprocess.run(
@@ -34,8 +32,8 @@ def get_ip_address_of_container(name):
 
 def clean_containers():
     logging.info('Cleaning containers...')
-    subprocess.run('docker stop -t 0 $(docker ps -q)', shell=True)
-    subprocess.run('docker rm $(docker ps -a -q)', shell=True)
+    subprocess.run('docker stop -t 0 $(docker ps -q) > /dev/null 2> /dev/null', shell=True)
+    subprocess.run('docker rm $(docker ps -a -q) > /dev/null 2> /dev/null', shell=True)
 
 
 MININET_URL = ''
@@ -93,6 +91,17 @@ def awareness_compute_potential_paths(throughput, threshold):
     
     return path
 
+def awareness_set_access_table_pinning(url, dpid, ip, port):
+    res = requests.patch(
+        url + '/awareness/access_table_entry_pinnings',
+        data=json.dumps({'pinnings': [{'dpid': dpid, 'ip': ip, 'port': port}]})
+    )
+
+def awareness_get_services():
+    return requests.get(AWARENESS_URL + '/awareness/services').json()['services']
+
+def awareness_get_switch_weights():
+    return requests.get(AWARENESS_URL + '/awareness/weights/switches').json()['switches']
 
 def mininet_start_iperf_servers():
     requests.post(
@@ -114,8 +123,9 @@ def mininet_ping():
 
 def log_event(file, name, data=''):
     j = json.dumps({'type': name, 'timestamp': time.time(), 'data': data})
-    logging.info('log_event(): ' + j)
+    logging.info('{}: {}'.format(name, data))
     file.write(j + '\n')
+    file.flush()
 
 # deprecated
 def log_awareness_paths(file):
@@ -132,6 +142,11 @@ def log_awareness_computed_path(file, throughput, threshold):
     log_event(file, 'awareness_computed_path',
               awareness_compute_potential_paths(throughput, threshold))
 
+def log_awareness_services(file):
+    log_event(file, 'awareness_services', awareness_get_services())
+
+def log_awareness_switch_weights(file):
+    log_event(file, 'awareness_switch_weights', awareness_get_switch_weights())
 
 def mininet_trigger_traffic(file, duration=3):
     logging.info('Triggering traffic for {}s'.format(duration))
@@ -157,24 +172,30 @@ def run_experiment(name, ext_agent_configs: list = [], ext_network_configs: list
         logging.info('Starting mininet and awareness...')
         subprocess.run([RUN_CONTAINERS, BASE_CONFIG, NETWORK_CONFIG, *ext_network_configs])
 
-        # time.sleep(3)
         MININET_URL = 'http://{}:8081'.format(
             get_ip_address_of_container('mininet'))
         AWARENESS_URL = 'http://{}:8080'.format(
-            get_ip_address_of_container('awareness'))
+            get_ip_address_of_container('awareness1'))
         logging.info('Mininet URL is ' + MININET_URL)
         logging.info('Awareness URL is ' + AWARENESS_URL)
         logging.info('Starting iperf servers...')
         mininet_start_iperf_servers()
         logging.info('Testing ping')
         mininet_ping()
-        time.sleep(3)
-        
+
+        # workaround the default route
+        if name.startswith('mesh_multisdn_'):
+            AWARENESS2_URL = 'http://{}:8080'.format(
+                get_ip_address_of_container('awareness2'))
+            awareness_set_access_table_pinning(AWARENESS_URL, 1, '10.0.0.2', 1)
+            awareness_set_access_table_pinning(AWARENESS2_URL, 2, '10.0.0.1', 1)
+            # wait for the old flow entries to expire
+            time.sleep(30)
 
         if run_agents:
             logging.info('Starting agents...')
             subprocess.run([RUN_CONTAINERS, '-w', 'run_name', name,
-                            BASE_CONFIG, AGENTS_CONFIG, *ext_agent_configs])
+                            BASE_CONFIG, AGENTS_CONFIG, *ext_network_configs, *ext_agent_configs, NO_NETWORK_CONFIG])
         
         # logging.info('Waiting for awareness to collect network information')
         # time.sleep(10)
@@ -183,16 +204,26 @@ def run_experiment(name, ext_agent_configs: list = [], ext_network_configs: list
             mininet_trigger_traffic(log, 10)
             throughput = log_awareness_throughput(log)
             log_awareness_computed_path(log, throughput, 80)
+            log_awareness_switch_weights(log)
+            log_awareness_services(log)
 
 
-def run_experiment_group(topo):
-    topo_file = os.path.join(MININET_TOPO_BASE_DIR, topo + '.yaml')
-    run_experiment(topo + '_plain_awareness', [], [topo_file])
-    run_experiment(topo + '_agents_without_overheating', [NO_SCKL_SM_CONFIG, NO_SCKL_DATA_CONFIG], [topo_file], True)
-    run_experiment(topo + '_agents_with_overheating', [NO_SCKL_SM_CONFIG], [topo_file], True)
+def run_experiment_group(network_topo, agent_topo):
+    network_topo_file = os.path.join(MININET_TOPO_BASE_DIR, network_topo + '.yaml')
+    agent_topo_file = os.path.join(AGENT_TOPO_BASE_DIR, agent_topo + '.yaml')
+    prefix = network_topo + '_ag' + agent_topo
+
+    # run_experiment(prefix + '_plain_awareness', [], [network_topo_file])
+    run_experiment(prefix + '_agents_without_overheating', [NO_SCKL_DATA_CONFIG, agent_topo_file], [network_topo_file], True)
+    run_experiment(prefix + '_agents_with_overheating', [agent_topo_file], [network_topo_file], True)
 
 if __name__ == '__main__':
-    #run_experiment_group('mesh')
-    run_experiment_group('custom')
-    #run_experiment_group('ring')
-    #run_experiment_group('complete_bipartite')
+    run_experiment_group('mesh', 'topo1')
+    run_experiment_group('mesh', 'topo2')
+    run_experiment_group('mesh', 'topo3')
+    run_experiment_group('mesh', 'topo4')
+    run_experiment_group('mesh_indirect', 'topo4_indirect')
+    run_experiment_group('mesh_multisdn', 'topo4')
+    run_experiment_group('custom', 'topo4')
+    run_experiment_group('ring', 'topo4')
+    run_experiment_group('complete_bipartite', 'topo4')
